23d56c691692d315dad33b18e3f4272607676d67
Rafael Rollo <rafaelrollo92@gmail.com>
2022-11-19 19:54:14 +0000

    Initial commit

---
+++ src/main/java/br/com/rollo/rafael/tuitrapi/TuitrApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rollo.rafael.tuitrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuitrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuitrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rollo/rafael/tuitrapi/TuitrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rollo.rafael.tuitrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuitrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
