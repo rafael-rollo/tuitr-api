9ad826fcd04343632347272fef4675c62d78f76c
Rafael Rollo <rafaelrollo92@gmail.com>
2022-12-05 10:34:10 +0000

    chore(swagger): update the auth method on the interactive documentation

--- src/main/java/br/com/rollo/rafael/tuitrapi/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/rollo/rafael/tuitrapi/configuration/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.rollo.rafael.tuitrapi.configuration;
 
+import br.com.rollo.rafael.tuitrapi.domain.posts.Post;
 import br.com.rollo.rafael.tuitrapi.domain.users.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +10,15 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.RequestParameterBuilder;
 import springfox.documentation.schema.ScalarType;
-import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -29,19 +33,42 @@ public class SwaggerConfiguration {
         logger.info("Creating Swagger docket configuration.");
         
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(SwaggerConfiguration.SCANNED_PACKAGE))
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
-                
-                .ignoredParameterTypes(User.class)  
-                .globalRequestParameters(
-                	Arrays.asList(
-                		new RequestParameterBuilder()
-                			.name("Authorization")
-                			.description("Authorization Bearer Token")
-                			.in(ParameterType.HEADER)
-                			.query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
-                			.build()));
+                .ignoredParameterTypes(User.class)
+                .ignoredParameterTypes(Post.class);
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "The Tuítr API",
+                "An API to serve clients inspired by the Twitter application.",
+                "1.0",
+                "urn:tos",
+                new Contact("Rafael Rollo", "", "rafaelrollo92@gmail.com"),
+                "MIT License",
+                "https://www.mit.edu/~amini/LICENSE.md",
+                Collections.emptyList());
+    }
+
+    private SecurityContext securityContext() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+
+        List<SecurityReference> references = Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+
+        return SecurityContext.builder().securityReferences(references).build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
 }
