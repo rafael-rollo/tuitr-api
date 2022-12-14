8c5474dad2e8c96b457fa07be086940d01f77604
Rafael Rollo <rafaelrollo92@gmail.com>
2022-12-14 12:48:15 +0000

    refactor(posts): fix on posts queries argument naming and documentation

--- src/main/java/br/com/rollo/rafael/tuitrapi/domain/posts/CustomizedPostRepositoryImpl.java
+++ src/main/java/br/com/rollo/rafael/tuitrapi/domain/posts/CustomizedPostRepositoryImpl.java
@@ -14,24 +14,31 @@ public class CustomizedPostRepositoryImpl implements CustomizedPostRepository {
 
     @Override
     public List<Post> findAllPostsOf(User user) {
-        String whereClausule = "where p.author.id = :userId";
-        return selectWithMultipleFetch(user, whereClausule);
+        String whereClause = "where p.author.id = :userId";
+        return selectWithMultipleFetch(user, whereClause);
     }
 
     @Override
     public List<Post> findAllPostsOfFollowedAccountsBy(User user) {
-        String whereClausule = "where p.author.id in " +
+        String whereClause = "where p.author.id in " +
                 "(select following.id from User u join u.following following where u.id = :userId)";
 
-        List<Post> posts = selectWithMultipleFetch(user, whereClausule);
+        List<Post> posts = selectWithMultipleFetch(user, whereClause);
 
         return posts;
     }
 
-    private List<Post> selectWithMultipleFetch(User user, String jpqlWhereClausule) {
+    /**
+     * Fetch posts by user with base planned query to avoid N+1 queries and multiple bag fetching problems.
+     * See the Vlad Mihalceas' answer on https://stackoverflow.com/a/51055523 to better understanding the implementation strategy
+     * @param user the user who triggers the action to load its posts, the followed people's posts, or any other condition.
+     * @param jpqlWhereClause the JPQL string that has a where clause condition to apply
+     * @return the list of posts fetched
+     */
+    private List<Post> selectWithMultipleFetch(User user, String jpqlWhereClause) {
         List<Post> posts = entityManager.createQuery("select distinct p from Post p " +
                 "left join fetch p.lovers " +
-                jpqlWhereClausule, Post.class)
+                jpqlWhereClause, Post.class)
                 .setParameter("userId", user.getId())
                 .setHint(QueryHints.PASS_DISTINCT_THROUGH, false)
                 .getResultList();
